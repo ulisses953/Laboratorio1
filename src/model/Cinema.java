5f9f39ff9abc6a16c769629f6a7e5369b23616a1
GabrielWalber <bielwalber@gmail.com>
2023-09-11 22:03:54 -0300

    desenvolvimento do método de sign up

--- src/model/Cinema.java
+++ src/model/Cinema.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Cinema {
     private long id;
     private String name;
+    private ArrayList<User> usuarios = new ArrayList<User>();
+    private ArrayList<Sessao> sessoes = new ArrayList<Sessao>();
 }
